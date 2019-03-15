eeeb04b37155c22322f9f5256101a2845a09312d
qzkml3 <qzkml3@gmail.com>
2019-03-15 15:54:31 +0900

    change folder name

--- test-language/requirejs/js/require_config.js
+++ test-language/requirejs/js/require_config.js
@@ -2,10 +2,10 @@ require.config({
 	paths: {
 		"jquery": "/_test/js/jquery/jquery-1.8.3.min",
 		"common": "/_test/js/common",
-        "inner": "/test_language/requirejs/js/inner",
-        "inner2": "/test_language/requirejs/js/inner2",
-        "outer": "/test_language/requirejs/js/outer",
-        "outer2": "/test_language/requirejs/js/outer2"
+        "inner": "/test-language/requirejs/js/inner",
+        "inner2": "/test-language/requirejs/js/inner2",
+        "outer": "/test-language/requirejs/js/outer",
+        "outer2": "/test-language/requirejs/js/outer2"
 	},
 	shim: {
         "common": {
