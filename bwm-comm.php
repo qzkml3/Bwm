49570b3e42f2fc25ce80e40fe164b9eddf362849
qzkml3 <qzkml3@gmail.com>
2019-03-22 09:43:43 +0900

    1

--- bwm-comm.php
+++ bwm-comm.php
@@ -1,41 +1,64 @@
 <?php
-	require_once Config::getPath('res') . '/class/Layout.php';
+	require_once Config::getPath('comm-res') . '/class/Layout.php';
 	
 	class Config {
 		//document
 		static function getSiteTitle() {
-			return "site_title";
+			return 'site_title';
 		}
 		
 		//db
 		static function getDbHost() {
-			return "db_host";
+			return 'db_host';
 		}
 		
 		static function getDbName() {
-			return "db_name";
+			return 'db_name';
 		}
 		
 		static function getDbId() {
-			return "db_id";
+			return 'db_id';
 		}
 		
 		static function getDbPwd() {
-			return "db_pwd";
+			return 'db_pwd';
 		}
 		
 		//site path
 		static function getPath($which) {
+			$root_path = "/bwm";
+			
+			$font_path = $root_path . '/menu';
+			$font_res_path = $font_path . '/res';
+			
+			$admin_path = $root_path . '/admin';
+			$admin_res_path = $admin_path . '/res';
+			
+			$comm_res_path = $root_path . '/comm-res';
+			
+			$rtn_path = '';
+			
 			switch ($which) {
-				default :
-					return "can't find path.";
+				case "root" :
+					$rtn_path = $root_path;
+					break;
 				case "front" :
-					return $_SERVER['DOCUMENT_ROOT'] . '/bwm/menu';
+					$rtn_path = $font_path;
+					break;
+				case "front-res" :
+					$rtn_path = $font_res_path;
+					break;
 				case "admin" :
-					return $_SERVER['DOCUMENT_ROOT'] . '/bwm/admin';
-				case "res" :
-					return $_SERVER['DOCUMENT_ROOT'] . '/bwm/res';
+					$rtn_path = $admin_path;
+					break;
+				case "admin-res" :
+					$rtn_path = $admin_res_path;
+					break;
+				case "comm-res" :
+					$rtn_path = $comm_res_path;
 			}
+			
+			return $_SERVER['DOCUMENT_ROOT'] . $rtn_path;
 		}
 	}
 ?>
