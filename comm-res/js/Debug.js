17a11f8811f97112459f79281ee29e5481e0c121
qzkml3 <qzkml3@gmail.com>
2019-04-01 15:12:07 +0900

    date-time

--- comm-res/js/Debug.js
+++ comm-res/js/Debug.js
@@ -4,7 +4,12 @@ Debug = {
 			$code = $(this);
 
 			$code.before("<xmp/>");
-			$code.prev().css("font-family", "ubuntu mono");
+			$code.prev().css({
+				'font-family': 'ubuntu mono'
+				, 'background': '#eee'
+				, 'padding-bottom': '1.5em'
+				, 'line-height': '1.5'
+			});
 			$code.prev().html($code.html())
 		});
 	}
