2df7a17687c9e47bef669e7d7e63c3e4cbacfa8b
qzkml3 <qzkml3@gmail.com>
2019-04-05 16:01:32 +0900

    containImg

--- comm-res/js/patch/OldPatcher.js
+++ comm-res/js/patch/OldPatcher.js
@@ -1,4 +1,49 @@
 OldPatcher = {
+	/*v1.0 이미지를 부모안에 포함시킨다. */
+	containImg: function (sel) {
+		$(sel).each(function (i) {
+			$img_w = $(this).parent();
+			$img = $(this);
+
+			if ($img_w.css("position") != "absolute" && $img_w.css("position") != "fixed") {
+				$img_w.css({"position": "relative"});
+			}
+
+			//$img.on("load, ready", function () {
+				$img = $(this);
+				var type;
+
+				if ($img.width() > $img.height()) {
+					type = "hori";
+				} else {
+					type = "verti";
+				}
+
+				$img.css({
+					"position":"absolute",
+					"top":"0",
+					"bottom":"0",
+					"left":"0",
+					"right":"0",
+					"margin":"auto"
+				});
+
+				if (type == "hori") {
+					$img.css({
+						"width": "100%",
+						"height": "auto"
+					});
+				} else {
+					$img.css({
+						"width": "auto",
+						"height": "100%"
+					});
+				}
+
+			//});
+		});
+	}
+	,
 	/* WIN7 IE11 맑은 고딕 상하 정렬 문제 패치*/
 	fixFontPos: function () {
 		if (UserAgent.WIN7) {
