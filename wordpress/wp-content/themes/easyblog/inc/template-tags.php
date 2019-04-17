252ae75aa1b7a7c0150c583b237d824d09bf2bf6
qzkml3 <qzkml3@gmail.com>
2019-04-17 17:30:52 +0900

    1

--- wordpress/wp-content/themes/easyblog/inc/template-tags.php
+++ wordpress/wp-content/themes/easyblog/inc/template-tags.php
@@ -49,7 +49,7 @@ function easyblog_entry_footer() {
 		/* translators: used between list items, there is a space after the comma */
 		$categories_list = get_the_category_list( esc_html__( ', ', 'easyblog' ) );
 		if ( $categories_list && easyblog_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'easyblog' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'easyblog' ) . '</span> / ', $categories_list ); // WPCS: XSS OK.
 		}
 
 		/* translators: used between list items, there is a space after the comma */
