2aeb7d44e9f1fcbf406d60458b1790e8da71954f
qzkml3 <qzkml3@gmail.com>
2019-05-17 18:22:34 +0900

    B_LAYOUT

---
+++ home/test-back-end/menu/error/error_report.php
@@ -0,0 +1,16 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/start-home.php';
+define('B_LAYOUT', 'test-back-end-sub');
+define('B_TITLE', 'error_report');
+define('B_DESC', '');
+BLayout::getHeader();
+?>
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+''
+?>
+
+<?php
+BLayout::getFooter();
+?>
