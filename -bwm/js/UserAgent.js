b5e746d5d8c61e317ea1d86328016d12947565c8
qzkml3 <qzkml3@gmail.com>
2019-05-21 14:02:57 +0900

    useragent

--- -bwm/js/UserAgent.js
+++ -bwm/js/UserAgent.js
@@ -1,66 +1,66 @@
 UserAgent = {
 	isWin7: function () {
-		if (StringUtil.hasString(navigator.userAgent, "Windows NT 6.1")) {
+		if (navigator.userAgent.match("Windows NT 6.1")) {
 			return true;
 		}
 	},
 	isIE: function () {
-		if (StringUtil.hasString(navigator.userAgent, "Trident")) {
+		if (navigator.userAgent.match("Trident")) {
 			return true;
 		}
 	},
 	isIE11: function () {
 		if (
-			StringUtil.hasString(navigator.userAgent, "Trident") &&
-			StringUtil.hasString(navigator.userAgent, "rv:11.0")
+			navigator.userAgent.match("Trident") &&
+			navigator.userAgent.match("rv:11.0")
 		) {
 			return true;
 		}
 	},
 	isIE10: function () {
-		if (StringUtil.hasString(navigator.userAgent, "MSIE 10")) {
+		if (navigator.userAgent.match("MSIE 10")) {
 			return true;
 		}
 	},
 	isIE9: function () {
-		if (StringUtil.hasString(navigator.userAgent, "MSIE 9")) {
+		if (navigator.userAgent.match("MSIE 9")) {
 			return true;
 		}
 	},
 	isIE8: function () {
-		if (StringUtil.hasString(navigator.userAgent, "MSIE 8")) {
+		if (navigator.userAgent.match("MSIE 8")) {
 			return true;
 		}
 	},
 	isIE7: function () {
-		if (StringUtil.hasString(navigator.userAgent, "MSIE 7")) {
+		if (navigator.userAgent.match("MSIE 7")) {
 			return true;
 		}
 	},
 	isIE6: function () {
-		if (StringUtil.hasString(navigator.userAgent, "MSIE 6")) {
+		if (navigator.userAgent.match("MSIE 6")) {
 			return true;
 		}
 	},
 	isQuirks: function () {
-		if (StringUtil.hasString(navigator.userAgent, "MSIE 5")) {
+		if (navigator.userAgent.match("MSIE 5")) {
 			return true;
 		}
 	},
 	isChrome: function () {
-		if (StringUtil.hasString(navigator.userAgent, "Chrome")) {
+		if (navigator.userAgent.match("Chrome")) {
 			return true;
 		}
 	},
 	isFireFox: function () {
-		if (StringUtil.hasString(navigator.userAgent, "Firefox")) {
+		if (navigator.userAgent.match("Firefox")) {
 			return true;
 		}
 	},
 	isIOS: function () {
 		if (
-			StringUtil.hasString(navigator.userAgent, "iPhone") ||
-			StringUtil.hasString(navigator.userAgent, "iPad")
+			navigator.userAgent.match("iPhone") ||
+			navigator.userAgent.match("iPad")
 		) {
 			return true;
 		}
