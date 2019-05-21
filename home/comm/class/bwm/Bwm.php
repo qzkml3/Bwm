48c4008d90c655de02e439dc74782edf89951b69
qzkml3 <qzkml3@gmail.com>
2019-05-21 11:31:49 +0900

    rename

--- home/comm/class/bwm/Bwm.php
+++ home/comm/class/bwm/Bwm.php
@@ -6,17 +6,20 @@
 		}
 
 		static function load() {
-			self::loadPrivate();
-			self::loadShare();
+			self::loadEashSiteClass();
+			self::loadCommSiteClass();
 		}
-		
-		static function loadPrivate() {
+
+		/**
+		 * indivisual site setting
+		 */
+		static function loadEashSiteClass() {
 			require_once 'BSite.php';
 			require_once 'BDb.php';
 			require_once 'BLayout.php';
 		}
 		
-		static function loadShare() {
+		static function loadCommSiteClass() {
 			require_once $_SERVER['DOCUMENT_ROOT'] . '/-bwm/class/BDate.php';
 			require_once $_SERVER['DOCUMENT_ROOT'] . '/-bwm/class/BFile.php';
 		}
