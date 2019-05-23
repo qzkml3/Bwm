a660d69530cd722682c66c126d6673d14de877a2
qzkml3 <qzkml3@gmail.com>
2019-05-23 14:12:08 +0900

    add ga code

--- home/comm/class/bwm/BSite.php
+++ home/comm/class/bwm/BSite.php
@@ -1,13 +1,19 @@
 <?php
-	define('B_SITE_NAME', 'Boulder\'s homepage');
-	define('B_TITLE_DELIMITER', ' : ');
-	
-	class BSite {
-		static function getPageTitle() {
-			if (B_TITLE) {
-				return B_TITLE . B_TITLE_DELIMITER . B_SITE_NAME;
-			} else {
-				return B_SITE_NAME;
-			}
+define('B_SITE_NAME', 'Boulder\'s homepage');
+define('B_TITLE_DELIMITER', ' : ');
+
+class BSite
+{
+	static function getPageTitle()
+	{
+		if (B_TITLE) {
+			return B_TITLE . B_TITLE_DELIMITER . B_SITE_NAME;
+		} else {
+			return B_SITE_NAME;
 		}
 	}
+
+	static function getGaCode() {
+		require_once $_SERVER['DOCUMENT_ROOT'] . '/home/comm/inc/code-ga.html';
+	}
+}
