577376c359e6c3a98bc8d1fbf3c2c71f5520f454
qzkml3 <qzkml3@gmail.com>
2019-05-30 11:44:12 +0900

    cookie today

--- -bwm/js/Cookie.js
+++ -bwm/js/Cookie.js
@@ -5,6 +5,14 @@
 - 파폭 개발자도구에서 테스트하면 새로고침을 하지 않아도 쿠키가 생성 삭제됨을 확인할 수 있어서 편함.
 * */
 Cookie = {
+	setCookOnlyToday: function (name, value) {
+		var d = new Date();
+		d.setHours(24);
+		d.setMinutes(0);
+		d.setSeconds(0);
+		document.cookie = name + "=" + escape(value) + "; path=/; expires=" + d.toUTCString() + ";"
+	}
+	,
 	setSessCook: function (name, value) {
 		this.setCookByDate(name, value, null);
 	}
@@ -12,13 +20,13 @@ Cookie = {
 	setCookByDate: function (name, value, expireDay) {
 		var todayDate = new Date();
 		todayDate.setDate(todayDate.getDate() + parseInt(expireDay));
-		document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";"
+		document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toUTCString() + ";"
 	}
 	,
 	setCookByHour: function (name, value, expireTime) {
 		var todayDate = new Date();
 		todayDate.setHours(todayDate.getHours() + parseInt(expireTime));
-		document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";"
+		document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toUTCString() + ";"
 	}
 	,
 	getVal: function (name) {
@@ -34,7 +42,6 @@ Cookie = {
 			if (i == 0) break;
 		}
 		return null;
-
 
 		function getCookieVal (offset) {
 			var endstr = document.cookie.indexOf(";", offset);
--- home/base/js/common.js
+++ home/base/js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	//OldPatcher.fixFontPos();
-	OldPatcher.setPlaceholder();
-	OldPatcher.setYoutubeInfo();
-	Ui.responsiveImgMap();
-	Ui.setAnimatedHash();
+	OldPatcher.setPlaceholder(); //old ie placeholder 적용
+	OldPatcher.setYoutubeInfo(); //old ie youtube 미지원 안내
+	Ui.responsiveImgMap(); // 반응형 이미지맵
+	Ui.setAnimatedHash(); //링크 클릭시 부드러운 이동
 });
