ef88d3d3b8a3d8e4d322fdb8c1475db33fcca42b
boulder <qzkml3@gmail.com>
2020-03-11 15:29:17 +0900

    test2

---
+++ test2/index.php
@@ -0,0 +1,5 @@
+<?php
+$_title = 'test'; $_layout = '';
+$_gnb = ''; $_lnb = '';
+?>
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/test2/load-page-test2.php';
---
+++ test2/load-page-test2.php
@@ -0,0 +1,12 @@
+<?php
+	define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT']);	
+	define('PRJ_URL', '/test');
+	define('PRJ_DIR', ROOT_DIR . PRJ_URL);
+	
+	require_once PRJ_DIR . '/inc/header.php';
+	
+	$v = $_SERVER['PHP_SELF'];
+	$v = str_replace('.php', '.html', $v);
+	require_once ROOT_DIR . $v;
+	
+	require_once PRJ_DIR . '/inc/footer.php';
