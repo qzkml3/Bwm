1a56760d36add2cad1c8f5800fcdbfbca69851fa
boulder <qzkml3@gmail.com>
2020-03-12 09:20:11 +0900

    header to layout

--- test-2/load-page-test-2.php
+++ test-2/load-page-test-2.php
@@ -6,10 +6,9 @@
 	define('PRJ_URL', '/test-2');
 	define('PRJ_DIR', ROOT_DIR . PRJ_URL);
 	
-	require_once PRJ_DIR . '/inc/header.php';
-	
 	$v = $_SERVER['PHP_SELF'];
 	$v = str_replace('.php', '.html', $v);
-	require_once ROOT_DIR . $v;
+	//require_once ROOT_DIR . $v;
+	$_view = ROOT_DIR . $v;
 	
-	require_once PRJ_DIR . '/inc/footer.php';
+	require_once PRJ_DIR . '/inc/layout/layout.php';
--- test-3/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$_title = 'test'; $_layout = '';
-$_gnb = ''; $_lnb = '';
-?>
-<?php require_once $_SERVER['DOCUMENT_ROOT'] . '/test-3/load-page-test-3.php';
