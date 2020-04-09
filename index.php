8f9bddc9ee2afc0a021872951a72ed1d824f35e3
boulder <qzkml3@gmail.com>
2020-04-10 00:00:17 +0900

    홈

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-메모공간
-프로젝트공간
-리뉴얼중...
-<a href="wordpress">내용 폭파할지 모르는 워드프레스...</a>
+<!doctype html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+<ul>
+	<li>메모공간</li>
+	<li>프로젝트 공간</li>
+	<li>테스트 공간</li>
+	<li>홈페이지 공간</li>
+</ul>
+<p>전부 파괴 후 리뉴얼중...</p>
+
+<div><a href="home">새로 시작하는 홈...</a></div>
+<div><a href="wordpress">내용 폭파할지 예정인 워드프레스...</a></div>
+</body>
+</html>
