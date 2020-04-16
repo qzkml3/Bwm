e5656287d93336d0baf5953d9305c6b11582a9be
boulder <qzkml3@gmail.com>
2020-04-16 17:00:59 +0900

    site id / remove

--- test/view/-layout/layout.php
+++ test/view/-layout/layout.php
@@ -1,9 +1,7 @@
 <!doctype html>
 <html lang="ko" class="test">
 <head>
-	<title>
-		<?=$b_page->getTitle();?>
-	</title>
+	<title><?=$b_page->getTitle();?></title>
 
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -12,10 +10,11 @@
 	<script src="/bwm/js/jquery/jquery-3.4.1.min.js"></script>
 	<script src="/bwm/js/b/b.js"></script>
 	
-	<link href="<?=$_site_conf->getCssUrl()?><?=$_site_conf->getSiteId()?>.css" rel="stylesheet">
-	<script src="<?=$_site_conf->getJsUrl()?><?=$_site_conf->getSiteId()?>.js"></script>
+	<link href="<?=$_site_conf->getCssUrl()?>/<?=$_site_conf->getSiteId()?>.css" rel="stylesheet">
+	<script src="<?=$_site_conf->getJsUrl()?>/<?=$_site_conf->getSiteId()?>.js"></script>
 </head>
 <body>
 <?php require_once $b_view; ?>
+
 </body>
 </html>
