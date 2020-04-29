27a3c3a71fef446ad50b2ca4de9d778dd9f87ed3
boulder <qzkml3@gmail.com>
2020-04-29 14:49:00 +0900

    test hangul

--- test/index.php
+++ test/index.php
@@ -1,6 +1,16 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/test/load-bwm.php';
-require_once '가.php';
+echo iconv( "EUC-KR", "UTF-8", '가.php');
+echo '<hr>';
+echo iconv( "UTF-8", "EUC-KR", '가.php');
+echo '<hr>';
+echo iconv( "UTF-8", "UTF-8", '가.php');
+echo '<hr>';
+
+
+require_once iconv( "UTF-8", "EUC-KR", '가.php');
+exit; 
+ 
 
 $b->layout('sub');
 $b->title('');
