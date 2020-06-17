19de02b6d26b1bbbf489a6e855c6fac74ed85d10
boulder <qzkml3@gmail.com>
2020-06-17 16:06:22 +0900

    id-pw

--- home/view/page.php
+++ home/view/page.php
@@ -2,7 +2,7 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/home/load-bwm.php';
 
 $b->setLayout('sub');
-$b->setTitle('page');
+$b->setTitle('Page');
 
 $cont_p = '가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 ';
 require_once $b->getPage();
