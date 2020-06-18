856265c351571f8c3f03b3a2d5c0a16704bcca8f
boulder <qzkml3@gmail.com>
2020-06-18 15:38:55 +0900

    b_hide_radio

---
+++ home/view/comp/pop/pop.php
@@ -0,0 +1,8 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/home/load-bwm.php';
+
+$b->setLayout('sub');
+$b->setTitle('레이어 팝업');
+
+require_once $b->getPage();
+?>
---
+++ home/view/member/join_form.php
@@ -0,0 +1,8 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/home/load-bwm.php';
+
+$b->setLayout('sub');
+$b->setTitle('회원가입 작성');
+
+require_once $b->getPage();
+?>
