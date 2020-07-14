ac641103dc4fe578d49898fef99520cf2a576f53
boulder <qzkml3@gmail.com>
2020-07-14 16:57:31 +0900

    1

--- home/view/page/page.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/home/load-bwm.php';
-
-$b->setLayout('sub');
-$b->setTitle('Page');
-
-$cont_p = '가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 가나다 ';
-require_once $b->getPage();
-?>
