f577f169e43a06159a259ea83940ce39903d8867
boulder <qzkml3@gmail.com>
2020-08-04 15:53:24 +0900

    _w, bt

--- test/view/fd/b_fd_w.php
+++ test/view/fd/b_fd_w.php
@@ -2,7 +2,7 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/home/load-bwm.php';
 
 $b->setLayout('sub');
-$b->setTitle('b_fd b_bt #field #button #필드 #버튼');
+$b->setTitle('#Form #폼 #Field #필드 #Button #버튼');
 
 require_once $b->getPage();
 ?>
