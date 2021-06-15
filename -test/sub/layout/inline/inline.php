8d80eea87b60e0c19539ebc825a3a280b77b49fa
boulder <qzkml3@gmail.com>
2021-06-15 10:02:48 +0900

    inline

--- -test/sub/layout/inline/inline.php
+++ -test/sub/layout/inline/inline.php
@@ -1,1 +1,8 @@
-tpl
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/home/load-bwm.php';
+
+$B->page->setTitle($B->getTitFromUrl());
+$B->page->setLayout('sub');
+
+require_once $B->page->getPage();
+?>
