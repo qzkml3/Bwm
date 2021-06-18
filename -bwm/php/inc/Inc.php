aa6ae2764a043b04a74503b2bc4c1cb6265445b6
boulder <qzkml3@gmail.com>
2021-06-18 16:57:58 +0900

    include

--- -bwm/php/inc/Inc.php
+++ -bwm/php/inc/Inc.php
@@ -2,10 +2,21 @@
 
 namespace Bwm\Inc;
 
+use Bwm\Str\Str;
+
 class Inc
 {
 
 	static function getRes($res)
+	{
+		if (Str::startsWith($res, '/')) {
+			return self::getResAbs($res);
+		} else {
+			return self::getResRel($res);
+		}
+	}
+
+	private static function getResAbs($res)
 	{
 		$file = $_SERVER['DOCUMENT_ROOT'] . $res;
 		$v = filemtime($file);
@@ -14,18 +25,13 @@ class Inc
 		return $res;
 	}
 
-	static function getResAbs($res)
-	{
-		return self::getRes($res);
-	}
-
-	static function getResRel($res)
+	private static function getResRel($res)
 	{
 		$path = $_SERVER['SCRIPT_NAME'];
 		$path = substr($path, 0, strrpos($path, '/') + 1);
 		$res = $path . $res;
-
-		return self::getRes($res);
+		
+		return self::getResAbs($res);
 	}
 	
 	static function test() {
