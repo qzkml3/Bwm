fdd0aa286b6a7dee333e31678debd7ba9852b41d
boulder <qzkml3@gmail.com>
2021-07-07 18:06:40 +0900

    svg img

--- -test/sub/img/img-size/img-size.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/home/load-bwm.php';
-
-$B->page->setTitle($B->getTitFromUrl());
-$B->page->setLayout('sub');
-$B->page->setTag('');
-
-require_once $B->page->getPage();
-?>
