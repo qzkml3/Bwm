10aa72c5eb1b1940303721e8aa5cefed7c74eb0b
boulder <qzkml3@gmail.com>
2021-08-31 09:25:10 +0900

    1

--- -test/sub/font/font-size.php
+++ -test/sub/font/font-size.php
@@ -1,7 +1,7 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/home/load-bwm.php';
 
-$B->page->setTitle('clone');
+$B->page->setTitle($B->getTitFromUrl());
 $B->page->setLayout('sub');
 
 require_once $B->page->getPage();
