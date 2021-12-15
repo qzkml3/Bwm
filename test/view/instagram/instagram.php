f0d37eccbe16ef656a7410385f49fc6f3c81062a
boulder <qzkml3@gmail.com>
2021-12-15 15:10:51 +0900

    instagram

---
+++ test/view/instagram/instagram.php
@@ -0,0 +1,8 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/home/load-bwm.php';
+
+$B->page->setTitle($B->getTitFromUrl());
+$B->page->setLayout('sub');
+
+require_once $B->page->getPage();
+?>
