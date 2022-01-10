a991248d17db4c8167bd41deb947510e988da78f
boulder <qzkml3@gmail.com>
2022-01-10 14:07:39 +0900

    swiper2

--- test/component/slider/swiper/swiper.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/home/load-bwm.php';
-
-$B->page->setTitle('swiper');
-$B->page->setLayout('sub');
-$B->page->setTag('#스와이퍼 #swiper');
-
-require_once $B->page->getPage();
-?>
